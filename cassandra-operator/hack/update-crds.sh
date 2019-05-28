1b1dbd2a8acf016c91caff3c5f7dc0d8780de8ef
Louis Taylor <louis@kragniz.eu>
2019-05-28 12:21:08 +0100

    Run munge script in makefile

--- cassandra-operator/hack/update-crds.sh
+++ cassandra-operator/hack/update-crds.sh
@@ -7,7 +7,8 @@ set -o pipefail
 REPO_ROOT="$(cd "$(dirname "$0")" && pwd -P)"/..
 cd "${REPO_ROOT}"
 
+crd_path="${REPO_ROOT}/kubernetes-resources/cassandra-operator-crd.yml"
+
 output="$(mktemp -d)"
 controller-gen crd:trivialVersions=true paths=./pkg/apis/... output:crd:dir="${output}"
-cp "${output}"/core.sky.uk_cassandras.yaml \
-    "${REPO_ROOT}/kubernetes-resources/cassandra-operator-crd.yml"
+go run "${REPO_ROOT}/hack/munge-crd.go" "${output}"/core.sky.uk_cassandras.yaml > "${crd_path}"
