b9d3413dd32ceb4ad434346d7098501bc439560a
Louis Taylor <louis@kragniz.eu>
2019-07-02 13:40:53 +0100

    Init controller-runtime-operator

---
+++ cassandra-operator/cmd/controller-runtime-operator/controller.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type reconcileCassandra struct {
+	// client can be used to retrieve objects from the APIServer.
+	client client.Client
+	log    logr.Logger
+}
+
+// Implement reconcile.Reconciler so the controller can reconcile objects
+var _ reconcile.Reconciler = &reconcileCassandra{}
+
+func (r *reconcileCassandra) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	fmt.Println(request)
+	return reconcile.Result{}, nil
+}
---
+++ cassandra-operator/cmd/controller-runtime-operator/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
+)
+
+var log = logf.Log.WithName("cassandra-operator")
+
+func main() {
+	flag.Parse()
+	entryLog := log.WithName("entrypoint")
+
+	// Setup a Manager
+	entryLog.Info("setting up manager")
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		entryLog.Error(err, "unable to set up overall controller manager")
+		os.Exit(1)
+	}
+
+	// Setup a new controller to reconcile ReplicaSets
+	entryLog.Info("Setting up controller")
+	c, err := controller.New("foo-controller", mgr, controller.Options{
+		Reconciler: &reconcileCassandra{client: mgr.GetClient(), log: log.WithName("reconciler")},
+	})
+	if err != nil {
+		entryLog.Error(err, "unable to set up individual controller")
+		os.Exit(1)
+	}
+
+	// Watch Cassandras and enqueue Cassandra object key
+	if err := c.Watch(&source.Kind{Type: &v1alpha1.Cassandra{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		entryLog.Error(err, "unable to watch Cassandras")
+		os.Exit(1)
+	}
+
+	// Watch StatefulSets and enqueue owning Cassandra key
+	if err := c.Watch(&source.Kind{Type: &appsv1.StatefulSet{}},
+		&handler.EnqueueRequestForOwner{OwnerType: &v1alpha1.Cassandra{}, IsController: true}); err != nil {
+		entryLog.Error(err, "unable to watch StatefulSets")
+		os.Exit(1)
+	}
+
+	// Watch Pods and enqueue owning Cassandra key
+	if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
+		&handler.EnqueueRequestForOwner{OwnerType: &v1alpha1.Cassandra{}, IsController: true}); err != nil {
+		entryLog.Error(err, "unable to watch Pods")
+		os.Exit(1)
+	}
+
+	entryLog.Info("starting manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		entryLog.Error(err, "unable to run manager")
+		os.Exit(1)
+	}
+}
