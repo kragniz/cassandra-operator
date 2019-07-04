b36afe3c20e3ec4fb7e64055cc0e454faa973ea1
Louis Taylor <louis@kragniz.eu>
2019-07-04 17:54:27 +0100

    Replace operator with controller-runtime based one

--- cassandra-operator/cmd/operator/main.go
+++ cassandra-operator/cmd/operator/main.go
@@ -1,118 +1,112 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
-	"k8s.io/client-go/kubernetes/scheme"
+	"flag"
 	"os"
-	"time"
 
-	log "github.com/sirupsen/logrus"
-	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/client/clientset/versioned"
-	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/operator"
-	"github.com/spf13/cobra"
-	"k8s.io/client-go/dynamic"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	kscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/apis/cassandra/v1alpha1"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/client/clientset/versioned"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/cluster"
+	"github.com/sky-uk/cassandra-operator/cassandra-operator/pkg/operator/operations"
 )
 
 var (
-	metricPollInterval   time.Duration
-	metricRequestTimeout time.Duration
-	logLevel             string
-	allowEmptyDir        bool
+	scheme = runtime.NewScheme()
+	log    = logf.Log.WithName("cassandra-operator")
 )
 
-var rootCmd = &cobra.Command{
-	Use:               "cassandra-operator",
-	Short:             "Operator for provisioning Cassandra clusters.",
-	PersistentPreRunE: handleArgs,
-	RunE:              startOperator,
+func init() {
+	v1alpha1.AddToScheme(scheme)
+	kscheme.AddToScheme(scheme)
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	flag.Parse()
+	logf.SetLogger(zap.Logger(false))
+	entryLog := log.WithName("entrypoint")
+
+	kubeConfig := config.GetConfigOrDie()
+
+	kubeClientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		entryLog.Error(err, "Unable to obtain clientset")
 		os.Exit(1)
 	}
-}
 
-func init() {
-	rootCmd.PersistentFlags().DurationVar(&metricPollInterval, "metric-poll-interval", 5*time.Second, "Poll interval between cassandra nodes metrics retrieval")
-	rootCmd.PersistentFlags().DurationVar(&metricRequestTimeout, "metric-request-timeout", 2*time.Second, "Time limit for cassandra node metrics requests")
-	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", log.InfoLevel.String(), "should be one of: debug, info, warn, error, fatal, panic")
-	rootCmd.PersistentFlags().BoolVar(&allowEmptyDir, "allow-empty-dir", false, "Set to true in order to allow creation of clusters which use emptyDir storage")
-}
+	cassandraClientset := versioned.NewForConfigOrDie(kubeConfig)
 
-func handleArgs(_ *cobra.Command, _ []string) error {
-	var isPositive = func(duration time.Duration) bool {
-		currentTime := time.Now()
-		return currentTime.Add(duration).After(currentTime)
-	}
-
-	if !isPositive(metricPollInterval) {
-		return fmt.Errorf("invalid metric-poll-interval, it must be a positive integer")
-	}
-
-	level, err := log.ParseLevel(logLevel)
+	// Setup a Manager
+	entryLog.Info("setting up manager")
+	mgr, err := manager.New(kubeConfig, manager.Options{Scheme: scheme})
 	if err != nil {
-		return fmt.Errorf("invalid log-level")
-	}
-	log.SetLevel(level)
-
-	return nil
-}
-
-func startOperator(_ *cobra.Command, _ []string) error {
-	operatorConfig := &operator.Config{
-		MetricRequestDuration: metricPollInterval,
-		MetricPollInterval:    metricPollInterval,
-		AllowEmptyDir:         allowEmptyDir,
-	}
-	log.Infof("Starting Cassandra operator with config: %+v", operatorConfig)
-
-	if err := v1alpha1.AddToScheme(scheme.Scheme); err != nil {
-		return fmt.Errorf("unable to register metadata for Cassandra resources: %v", err)
+		entryLog.Error(err, "unable to set up overall controller manager")
+		os.Exit(1)
 	}
 
-	kubernetesConfig := kubernetesConfig()
-	op := operator.New(kubernetesClient(kubernetesConfig), cassandraClient(kubernetesConfig), dynamicClient(kubernetesConfig), operatorConfig)
-	op.Run()
+	clusters := make(map[string]*cluster.Cluster)
 
-	return nil
-}
+	eventRecorder := cluster.NewEventRecorder(kubeClientset)
+	clusterAccessor := cluster.NewAccessor(kubeClientset, cassandraClientset, eventRecorder)
 
-func kubernetesConfig() *rest.Config {
-	config, err := rest.InClusterConfig()
+	receiver := operations.NewEventReceiver(
+		clusters,
+		clusterAccessor,
+		nil,
+		eventRecorder,
+	)
+
+	// Setup a new controller to reconcile ReplicaSets
+	entryLog.Info("Setting up controller")
+	c, err := controller.New("cassandra", mgr, controller.Options{
+		Reconciler: &reconcileCassandra{
+			previousCassandras: map[string]*v1alpha1.Cassandra{},
+			client:             mgr.GetClient(),
+			log:                log.WithName("reconciler"),
+			receiver:           receiver,
+		},
+	})
 	if err != nil {
-		log.Warnf("Falling back to default client config: %v", err)
-
-		apiConfig, err := clientcmd.NewDefaultClientConfigLoadingRules().Load()
-
-		if err != nil {
-			log.Fatalf("Unable to obtain cluster config: %v", err)
-		}
-
-		config, err = clientcmd.NewDefaultClientConfig(*apiConfig, &clientcmd.ConfigOverrides{}).ClientConfig()
-		if err != nil {
-			log.Fatalf("Unable to obtain cluster client config: %v", err)
-		}
+		entryLog.Error(err, "unable to set up individual controller")
+		os.Exit(1)
 	}
-	return config
-}
 
-func kubernetesClient(config *rest.Config) *kubernetes.Clientset {
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalf("Unable to obtain clientset: %v", err)
+	// Watch Cassandras and enqueue Cassandra object key
+	if err := c.Watch(&source.Kind{Type: &v1alpha1.Cassandra{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		entryLog.Error(err, "unable to watch Cassandras")
+		os.Exit(1)
 	}
-	return clientset
-}
 
-func cassandraClient(config *rest.Config) *versioned.Clientset {
-	return versioned.NewForConfigOrDie(config)
-}
+	// Watch StatefulSets and enqueue owning Cassandra key
+	if err := c.Watch(&source.Kind{Type: &appsv1.StatefulSet{}},
+		&handler.EnqueueRequestForOwner{OwnerType: &v1alpha1.Cassandra{}, IsController: true}); err != nil {
+		entryLog.Error(err, "unable to watch StatefulSets")
+		os.Exit(1)
+	}
 
-func dynamicClient(config *rest.Config) dynamic.Interface {
-	return dynamic.NewForConfigOrDie(config)
+	// Watch Pods and enqueue owning Cassandra key
+	if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
+		&handler.EnqueueRequestForOwner{OwnerType: &v1alpha1.Cassandra{}, IsController: true}); err != nil {
+		entryLog.Error(err, "unable to watch Pods")
+		os.Exit(1)
+	}
+
+	entryLog.Info("starting manager")
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		entryLog.Error(err, "unable to run manager")
+		os.Exit(1)
+	}
 }
