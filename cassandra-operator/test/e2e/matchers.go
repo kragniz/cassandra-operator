d423bc1e74d542eca32906fc27362a8a7ea5f883
Louis Taylor <louis@kragniz.eu>
2019-07-11 16:02:40 +0000

    Revert "Increase lastTimestampIsCloseTo threshold"
    This reverts commit 36b1a429f03012114c8b021fe6f2ec7c0f5d15f9.

--- cassandra-operator/test/e2e/matchers.go
+++ cassandra-operator/test/e2e/matchers.go
@@ -148,7 +148,7 @@ func (matcher *haveEvent) Match(actual interface{}) (success bool, err error) {
 }
 
 func (matcher *haveEvent) lastTimestampIsCloseTo(eventTimestamp v1.Time) bool {
-	return math.Abs(eventTimestamp.Sub(*matcher.expected.LastTimestampCloseTo).Seconds()) < 120
+	return math.Abs(eventTimestamp.Sub(*matcher.expected.LastTimestampCloseTo).Seconds()) < 45
 }
 
 func (matcher *haveEvent) FailureMessage(actual interface{}) (message string) {
